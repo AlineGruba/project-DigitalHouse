1dfeedc255ec6cb7424ff98cc7e1b12d718e6052
Aline Gruba <alinegruba@Alines-MacBook-Air.local>
2022-03-01 20:05:40 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let dominio = "digitalhouse.com.ar"
+    
+console.log(dominio.replace("http://www.digitalhouse.com.ar"));
+
+
+
+//node digital.js
+
+
+function dominio2 (digitalhouse){
+    return 12+dominio2
+
+    
+}
+
+console.log(dominio2);
